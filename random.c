3986b6dd3e8fda7a2fd3a2125d2c8abbb34dc3d2
Rouzbeh Majidi <rouzbeh.majidi@hotmail.com>
2018-09-18 21:53:13 -0400

    Improved PRNG

--- random.c
+++ random.c
@@ -1,20 +1,22 @@
 #include <stdio.h>
 #include <time.h>
 
-int A = 61;
+int A = 997;
 int B = 0;
-int M = 997;
+int M_int = 2147483647;
+float M_float = 2147483647.0F;
 int SEED = 1;
 
 int count = 0;
 
 int generateNext(){
-    SEED = (A * SEED + B) % M;
+    SEED = (A * SEED + B) % M_int;
     return SEED;
 }
 
+
 int main() {
-    SEED =  -1*time(NULL);
+    SEED =  time(NULL);
 
     FILE *file = fopen("random_out.dat", "w");
     if (file == NULL)
@@ -23,11 +25,11 @@ int main() {
         return 1;
     }
 
-    while(count < 1000){
+    while(count < 1000000){
         int B = count;
-        int number = generateNext();
-        printf("%d\n", number);
-        fprintf(file,"%d\n", number);
+        float number = generateNext() / M_float;
+        fprintf(file,"%f\n", number);
+        //printf("%f\n", number);
         //getchar(); 
         count ++;
     }
