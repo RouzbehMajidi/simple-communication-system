0beb921d0f0cd6f913cd66f1a09ad90e4ce0146f
Rouzbeh Majidi <rouzbeh.majidi@hotmail.com>
2018-09-25 17:41:50 -0400

    Added Gaussian Generator

--- random.c
+++ random.c
@@ -1,42 +1,38 @@
-#include <stdio.h>
-#include <time.h>
+#include "random.h"
 
 int A = 997;
 int B = 0;
 int M_int = 2147483647;
 float M_float = 2147483647.0F;
-int SEED = 1;
+int SEED = -1;
 
 int count = 0;
 
-int generateNext(){
-    SEED = (A * SEED + B) % M_int;
-    return SEED;
+float generateNext(){
+    if(SEED == -1){
+        SEED =  time(NULL);
+    }
+    SEED = ((A * SEED + B) % M_int);
+    return SEED / M_float;
 }
 
+// int main() {
+//     FILE *file = fopen("random_out.dat", "w");
+//     if (file == NULL)
+//     {
+//         printf("Error opening file!\n");
+//         return 1;
+//     }
 
-int main() {
-    SEED =  time(NULL);
+//     while(count < 10000000){
+//         int B = count;
+//         float number = generateNext();
+//         fprintf(file,"%f\n", number);
+//         count ++;
+//     }
 
-    FILE *file = fopen("random_out.dat", "w");
-    if (file == NULL)
-    {
-        printf("Error opening file!\n");
-        return 1;
-    }
-
-    while(count < 1000000){
-        int B = count;
-        float number = generateNext() / M_float;
-        fprintf(file,"%f\n", number);
-        //printf("%f\n", number);
-        //getchar(); 
-        count ++;
-    }
-
-    fclose(file);
-    return 0;
-    
-}
+//     fclose(file);
+//     return 0;
+// }
 
 
