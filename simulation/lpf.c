f69771aba9ea819ea61be62a9c5f24aa6b8f5bf6
Rouzbeh Majidi <rouzbeh.majidi@hotmail.com>
2018-11-22 13:26:58 -0500

    Updated LPF code

--- simulation/lpf.c
+++ simulation/lpf.c
@@ -2,14 +2,66 @@
 #include <stdio.h>
 #include <math.h>
 
-double gammaFactor(double f_3dB, int N){
-    double gamma = 4*M_PI*f_3dB*N;
-    return gamma / (1 - exp(-gamma));
+const int N = 10; //Samples per symbol
+const int K = 50; //Number of discrete samples
+const int T = 100;
+const double F_3DB  = 1/ (double) T; //3dB cutoff of low pass filter
+
+double continuousResponse[K];
+double discreteResponse[K];
+
+const double ALPHA() {
+    return 2*M_PI*F_3DB;
+}
+
+const double LAMBDA(){
+    return (2*M_PI*F_3DB*T)/N;
+}
+
+const double BETA() {
+    return exp(-LAMBDA());
+}
+
+const double GAMMA(){
+    return (4*M_PI*F_3DB*T)/N;
+}
+
+const double GAMMA_FACTOR(){
+    double gamma = GAMMA(); 
+    return  gamma / (1 - exp(-gamma));
+}
+
+double filterStepResponse(double t){
+    return 1 - exp( -ALPHA()*t);
 }
 
 int main(){
+    FILE *continuousOutput = fopen("data/lpf_continuous.dat", "w");
+    FILE *discreteOutput = fopen("data/lpf_discrete.dat", "w");
+    
+    if (continuousOutput == NULL || discreteOutput == NULL) {
+        printf("Error opening file!\n");
+        return 1;
+    }
 
-    printf("%0.10lf\n", gammaFactor(1,10));
+    printf("Gamma Factor : %0.10lf\n", GAMMA_FACTOR());
 
+    int X = 1; //Unit step is always 1 for t or n > 0
+
+    //Continuous Response
+    for(int i = 0; i < K; i ++){
+        fprintf(continuousOutput, "%0.2lf\n", filterStepResponse(i));
+    }
+
+    //Discrete Response
+    for(int i = 0; i < (K - 1); i ++){
+        discreteResponse[i + 1] = BETA()*discreteResponse[i] + LAMBDA()*X;
+    }
+
+    for(int i = 0; i < K; i ++){
+        fprintf(discreteOutput, "%0.2lf\n", discreteResponse[i]);
+    }
+
+    printf("Simulation Complete\n");
     return 0;
 }
